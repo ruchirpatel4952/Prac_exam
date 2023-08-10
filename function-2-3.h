d0ccb1587d5ff026ad6585a143bdcd34216f6393
Ruchir <ruchirpatel2000@gmail.com>
2023-08-10 14:16:30 +0930

    done 3

--- function-2-3.h
+++ function-2-3.h
@@ -1,10 +1,9 @@
-// functions.h
-#ifndef FUNCTIONS_2_3H
-#define FUNCTIONS_2_3H
+// function-2-3.h
+#ifndef FUNCTION_2_3_H
+#define FUNCTION_2_3_H
 
 bool is_array_palindrome(int integers[], int length);
 int sum_integers(int integers[], int length);
 int palindrome_sum(int integers[], int length);
 
 #endif
-
